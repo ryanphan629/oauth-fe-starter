ba3e8d98d3d1032c64e382f2d17d2f533caf1594
Ryan Phan <ryanwithcodes@gmail.com>
2023-04-19 16:57:52 +0700

    ref - refactor

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
-interface ZButtonProps { }
-
-const Header: React.FC<ZButtonProps> = () => {
+const Header: React.FC = () => {
   return (
     <header className="font-bold text-center w-full p-4 text-4xl">
       OAuth Learning Starter
