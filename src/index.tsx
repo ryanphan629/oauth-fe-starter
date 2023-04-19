904c1d65ba87ed1d13a6ea680020c672b0e56785
Ryan Phan <ryanwithcodes@gmail.com>
2023-04-19 17:27:58 +0700

    ref - add chakra ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 )
